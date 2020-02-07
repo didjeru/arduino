1b803c646c3d2e9e9809bad783324573d41a2bb9
Di <55582048+didjeru@users.noreply.github.com>
2020-02-07 21:34:03 +0700

    Update CO2_Hum_Temp.ino

--- CO2_Hum_Temp.ino
+++ CO2_Hum_Temp.ino
@@ -1,54 +1,55 @@
 #include <SoftwareSerial.h>
 #include <Wire.h>
-#include <SSD1306Ascii.h>
-#include <SSD1306AsciiWire.h>
 #include <DHT.h>
+#include "SSD1306Ascii.h"
+#include "SSD1306AsciiWire.h"
 
-#define DHTPIN A0
-#define DHTTYPE DHT22
+#define MH_PIN_RX 8
+#define MH_PIN_TX 9
+#define DHT_PIN A0
+#define PIEZO_PIN A3
+#define DHT_TYPE DHT22
 #define I2C_ADDRESS 0x3C
-#define PIEZOPIN A3
+#define PERIOD_SEC 10
 #define FONT ZevvPeep8x16
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHT_PIN, DHT_TYPE);
 SSD1306AsciiWire display;
-SoftwareSerial mySerial(8, 9);
+SoftwareSerial mhSerial(MH_PIN_RX, MH_PIN_TX);
 
 byte cmd[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};
-unsigned char response[9];
+byte response[9];
 
 long uptime = 0;
 
 void setup() {
+  delay(3000);
   Serial.begin(9600);
-  mySerial.begin(9600);
+  mhSerial.begin(9600);
 
   Wire.begin();
+  Wire.setClock(400000L);
   display.begin(&Adafruit128x32, I2C_ADDRESS);
-  display.set400kHz();
   display.setFont(FONT);
-  //display.println("setup::init()");
-
   dht.begin();
 }
 
 void loop()
 {
   //MH-Z19B
-  mySerial.write(cmd, 9);
+  mhSerial.write(cmd, 9);  
   memset(response, 0, 9);
-  mySerial.readBytes(response, 9);
-  int i;
+  mhSerial.readBytes(response, 9);
   byte crc = 0;
-  for (i = 1; i < 8; i++) crc += response[i];
+  for (int i = 0; i < 8; i++) crc += response[i];
   crc = 255 - crc;
-  crc++;
 
   //DHT
   float hum = dht.readHumidity();
   float temp = dht.readTemperature();
 
   //MAIN
+  display.clear();
   if ( !(response[0] == 0xFF && response[1] == 0x86 && response[8] == crc) ) {
     Serial.println("CRC error: " + String(crc) + " / " + String(response[8]));
     display.println("MH-Z19 CRC error");
@@ -58,7 +59,6 @@ void loop()
     unsigned int ppm = (256 * responseHigh) + responseLow;
 
     Serial.println("Uptime: " + String(uptime / 60) + "min., CO2: " + String(ppm) + "ppm, Hum: " + String(hum) + "%, Temp: " + String(temp) + "C");
-
     if (ppm <= 400 || ppm > 4900 || isnan(hum) || isnan(temp)) {
       display.println("Sensor error!");
     } else {
@@ -71,16 +71,17 @@ void loop()
       }
       else if (ppm < 1600) {
         display.println("CO2:" + String(ppm) + " is BAD!");
-        //tone(PIEZOPIN, 1000, 500);
+        tone(PIEZO_PIN, 100, 50);
       }
       else if (ppm < 2500) {
         display.println("CO2:" + String(ppm) + " CRITIC!");
+        tone(PIEZO_PIN, 500, 50);
       }
       else {
         display.println("CO2:" + String(ppm) + " !ALERT!");
       }
     }
   }
-  delay(10000);
-  uptime += 10;
+  delay(PERIOD_SEC*1000);
+  uptime += PERIOD_SEC;
 }
